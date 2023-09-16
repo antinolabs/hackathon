c0ca236784528f14c8c2e7c4d3d1020d84dcc600
rana <rishab@my3dmeta.com>
2023-09-16 20:18:24 +0530

    profile add

---
+++ src/apis/profileApi.js
@@ -0,0 +1,28 @@
+import { useMutation, useQueries, useQuery } from "@tanstack/react-query";
+import axios, { Axios } from "axios";
+import useAxiosInstance from "../utils/useAxiosInstance";
+
+const getUserProfile = async (id) => {
+  return await useAxiosInstance.post(
+    ` https://f9c3-103-240-193-66.ngrok-free.app/user/get`
+  );
+};
+
+const updateUserProfile = async (payload) => {
+  return await useAxiosInstance.post(
+    ` https://f9c3-103-240-193-66.ngrok-free.app/user/update`,
+    payload
+  );
+};
+
+export const useGetUserProfile = () =>
+  useQuery({
+    queryKey: ["user-details"],
+    queryFn: () => getUserProfile(),
+  });
+
+export const useUpdateUserProfile = () => {
+  useMutation({
+    mutationFn: () => updateUserProfile(payload),
+  });
+};
