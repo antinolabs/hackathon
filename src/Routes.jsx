10896ffd14aa9c63a342fffc990df1c0480502d7
rana <rishab@my3dmeta.com>
2023-09-16 13:45:45 +0530

    login

---
+++ src/Routes.jsx
@@ -0,0 +1,31 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+import { Route, Routes } from "react-router-dom";
+
+import { Sidebar, Navbar } from "./components";
+import { CampaignDetails, CreateCampaign, Home, Profile } from "./pages";
+
+export const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<SigninForm />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+        </Routes>
+      </div>
+    </>
+  )
+);
