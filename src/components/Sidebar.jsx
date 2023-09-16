9f0c53e1bcb2a0553cb812ae4a20e02737f7629c
puranantino <puran.d@antino.io>
2023-09-16 19:25:41 +0530

    ui

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -35,10 +35,12 @@ const Sidebar = () => {
         <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
       </Link>
 
-      <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
+      <div className="flex-1 flex flex-col justify-between items-start bg-[#1c1c24] rounded-[20px] w-[150px] py-4  mt-12">
         <div className="flex flex-col justify-center items-center gap-3">
           {navlinks.map((link) => (
-            <Icon
+            <div className="flex justify-end  items-center ">
+              
+                <Icon
               key={link.name}
               {...link}
               isActive={isActive}
@@ -53,10 +55,14 @@ const Sidebar = () => {
                 }
               }}
             />
+            <p className="text-white">{link?.label}</p>
+            </div>
+          
           ))}
         </div>
 
         <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
+        
       </div>
     </div>
   );
--- src/constants/index.js
+++ src/constants/index.js
@@ -13,38 +13,43 @@ export const navlinks = [
     name: "dashboard",
     imgUrl: dashboard,
     link: "/",
+    label :"Dashboard"
   },
   {
     name: "campaign",
     imgUrl: createCampaign,
     link: "/create-campaign",
+    label:"Campaign"
   },
   {
     name: "payment",
     imgUrl: payment,
     link: "/",
-    disabled: true,
+   
+    label:"Payment"
   },
   {
     name: "withdraw",
     imgUrl: withdraw,
     link: "/",
-    disabled: true,
+
+    label:"Withdraw"
   },
   {
     name: "profile",
     imgUrl: profile,
     link: "/profile",
+    label:"Profile"
   },
   {
     name: "logout",
     imgUrl: logout,
     link: "/signin",
+    label:"Log-out",
     onclick: (navigate) => {
       Cookies.remove("token");
       console.log("logout");
       navigate("/signin");
     },
-    isLogin: true,
   },
 ];
